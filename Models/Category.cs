ef71ea7862521b19bbd76cdc63058482f79532b7
theplas <70925661+theplas@users.noreply.github.com>
2020-11-11 08:17:35 +0700

    update

---
+++ Models/Category.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP_darklook.Models
+{
+
+        public class Category
+        {
+            public int Id { get; set; }
+
+            [DisplayName(" Category Name ")]
+            [Required(ErrorMessage = " Category name cannot be left blank ")]
+            [MinLength(3, ErrorMessage = " Category name must be at least 3 characters ")]
+            public string CategoryName { get; set; }
+
+            [DisplayName(" Description of category ")]
+
+            public List<Posts> Posts { get; set; }
+        }
+    }
+
---
+++ Models/Posts.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP_darklook.Models
+{
+    public class Posts 
+    {
+        
+
+            public int Id { get; set; }
+            public string Title { get; set; }
+            public string Content { get; set; }
+
+            public string ImageUrl { get; set; }
+            public DateTime DateTime { get; set; }
+            public int CategoryId { get; set; }
+            public Category Category { get; set; }
+
+
+        }
+    }
